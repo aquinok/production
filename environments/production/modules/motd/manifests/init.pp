e4ec5e6e3b17984a9e942e6a6761b26bbeb6b9e9
Nick Guyer <nick.guyer@rackspace.com>
2017-07-20 03:17:55 -0500

    Fixed mode

--- environments/production/modules/motd/manifests/init.pp
+++ environments/production/modules/motd/manifests/init.pp
@@ -2,7 +2,7 @@ class motd ($set_motd = "WARNING: hiera data not set") {
   file { '/etc/motd':
     ensure  => file,
     content => $set_motd,
-    mode    => 0644,
+    mode    => '0644',
     owner   => root,
     group   => root,
   }
